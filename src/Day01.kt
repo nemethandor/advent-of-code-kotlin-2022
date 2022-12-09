0d3a50e14404e19ae6d7af9d79b5b43711a95cf7
andnemeth <v-andnemeth@expediagroup.com>
2022-12-09 19:57:29 +0100

    Day01: part one: How many total Calories is that Elf carrying?

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum: Int = 0
+        var elfCounter = 1
+        val elfsCalories = mutableMapOf<Int, Int>()
+        for (text in input) {
+            if (text.isEmpty()) {
+                elfsCalories.put(elfCounter, sum)
+                elfCounter++
+                sum = 0
+            } else {
+                sum += text.toInt()
+            }
+        }
+        return elfsCalories.maxOf { (_, calories) -> calories }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
