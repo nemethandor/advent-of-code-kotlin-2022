458641a2a2231aa75d3bf3cd00e7918ae8541fac
andnemeth <v-andnemeth@expediagroup.com>
2022-12-09 23:50:48 +0100

    Day01: Using video solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,43 +1,28 @@
+import java.io.File
+
 // https://adventofcode.com/2022/day/1
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        var sum: Int = 0
-        var elfCounter = 1
-        val elfsCalories = mutableMapOf<Int, Int>()
-        for (text in input) {
-            if (text.isEmpty()) {
-                elfsCalories.put(elfCounter, sum)
-                elfCounter++
-                sum = 0
-            } else {
-                sum += text.toInt()
-            }
-        }
-        return elfsCalories.maxOf { (_, calories) -> calories }
+
+    fun parseInput(input: String) = input.split("\r\n\r\n").map { elf ->
+        elf.lines().sumOf { it.toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        var sum: Int = 0
-        var elfCounter = 1
-        val elfsCalories = mutableMapOf<Int, Int>()
-        for (text in input) {
-            if (text.isEmpty()) {
-                elfsCalories.put(elfCounter, sum)
-                elfCounter++
-                sum = 0
-            } else {
-                sum += text.toInt()
-            }
-        }
-        return elfsCalories.toList().sortedByDescending { (_, calories) -> calories }.take(3).sumOf { it.second }
-    }
+    fun getNElvesCalories(input: String, n: Int) = parseInput(input)
+        .sortedByDescending { it }
+        .take(n)
+        .sum()
+
+    fun part1(input: String): Int = getNElvesCalories(input, 1)
+
+    fun part2(input: String): Int = getNElvesCalories(input, 3)
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 24000)
+    val testInput = File("src/Day01_test.txt").readText()
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = File("src/Day01.txt").readText()
     println(part1(input))
     println(part2(input))
 }
