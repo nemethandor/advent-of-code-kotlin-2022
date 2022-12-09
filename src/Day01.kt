19b95a1732bf169935a85eb63a9d12828f2e9767
andnemeth <v-andnemeth@expediagroup.com>
2022-12-09 20:12:02 +0100

    Day01: part two: How many Calories are first three Elves carrying in total?

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+// https://adventofcode.com/2022/day/1
+
 fun main() {
     fun part1(input: List<String>): Int {
         var sum: Int = 0
@@ -16,12 +18,24 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var sum: Int = 0
+        var elfCounter = 1
+        val elfsCalories = mutableMapOf<Int, Int>()
+        for (text in input) {
+            if (text.isEmpty()) {
+                elfsCalories.put(elfCounter, sum)
+                elfCounter++
+                sum = 0
+            } else {
+                sum += text.toInt()
+            }
+        }
+        return elfsCalories.toList().sortedByDescending { (_, calories) -> calories }.take(3).sumOf { it.second }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 24000)
+//    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
