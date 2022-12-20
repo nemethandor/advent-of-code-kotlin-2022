42ce114850d08a741fae3fdd572506fdc3e43a3f
andnemeth <v-andnemeth@expediagroup.com>
2022-12-20 14:26:39 +0100

    Day02: part02: Changed tactic

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,27 +1,34 @@
 // https://adventofcode.com/2022/day/2
 
-private val whoBeatsWho = mapOf("X" to "C", "Y" to "A", "Z" to "B")
+private val whoBeatsWho = mapOf("A" to "C", "B" to "A", "C" to "B")
 private val pointsForTool = mapOf("X" to 1, "Y" to 2, "Z" to 3)
 private val equivalentTools = mapOf("A" to "X", "B" to "Y", "C" to "Z")
 
 fun main() {
 
-    fun isWinner(opponentPlay: String, yourPlay: String): Boolean = opponentPlay == whoBeatsWho[yourPlay]
-
-    fun isDraw(opponentPlay: String, yourPlay: String): Boolean = yourPlay == equivalentTools[opponentPlay]
-
-    fun getGameResult(opponentPlay: String, yourPlay: String): Int = when {
-        isWinner(opponentPlay, yourPlay) -> 6
-        isDraw(opponentPlay, yourPlay) -> 3
+    fun getGameResult(tactic: String): Int = when (tactic) {
+        "Z" -> 6
+        "Y" -> 3
         else -> 0
+    }
+
+    fun findYourTool(opponentPlay: String, tactic: String): String? {
+        val looser = whoBeatsWho[opponentPlay]
+        val winner = whoBeatsWho[looser]
+        val tactic = when (tactic) {
+            "Y" -> opponentPlay
+            "X" -> looser
+            else -> winner
+        }
+        return equivalentTools[tactic]
     }
 
     fun getTotalScore(input: String): Int {
 
         return input.lines().sumOf { game ->
             val plays = game.split(" ")
-            val gameResult = getGameResult(plays[0], plays[1])
-            val extraPoints = plays[1]?.let { pointsForTool[it] } ?: 0
+            val gameResult = getGameResult(plays[1])
+            val extraPoints = findYourTool(plays[0], plays[1])?.let { pointsForTool[it] } ?: 0
             gameResult.plus(extraPoints)
         }
     }
@@ -29,7 +36,7 @@ fun main() {
     val testInput = readInputFile("Day02_test")
     val testResult = getTotalScore(testInput)
     println(testResult)
-    check(testResult == 15)
+    check(testResult == 12)
 
     val input = readInputFile("Day02")
     val result = getTotalScore(input)
